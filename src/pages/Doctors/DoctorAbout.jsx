91d88a4f59c89f78ae1c706166f2b4d01d7986bc
Terence Faid JABO <faidterence@outlook.com>
2023-10-20 18:21:23 +0200

    Added Functionality to formulate dates

--- src/pages/Doctors/DoctorAbout.jsx
+++ src/pages/Doctors/DoctorAbout.jsx
@@ -56,7 +56,25 @@ export const DoctorAbout = () => {
         </ul>
       </div>
 
-       
+      <div className="mt-12">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold ">
+          Experience
+        </h3>
+
+        <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColor tex-[15px] leading-6 font-semibold">
+              {formateDate("07-04-2010")} - {formateDate("08-13-2014")}
+            </span>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Senior Surgeon 
+            </p>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Hospital of Cincinatti , Cincinatti
+            </p>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
