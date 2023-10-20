43e586ad497a89fdcc805bb1566471d683da86f6
Terence Faid JABO <faidterence@outlook.com>
2023-10-20 17:50:57 +0200

    Add doctor's detail page

---
+++ src/pages/Doctors/DoctorAbout.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { formateDate } from "../../utilities/formulateDate";
+export const DoctorAbout = () => {
+  return (
+    <div>
+      <div>
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2">
+          About Of
+          <span className="text-irisBlueColor font-bold text-[24px] leading-9">
+            Martin ISHIMWE
+          </span>
+        </h3>
+        <p className="text__para">
+          A surgeon doctor is a highly skilled medical professional specializing
+          in surgical procedures. They diagnose, treat, and manage medical
+          conditions through precise surgical interventions, playing a crucial
+          role in healthcare. Surgeons undergo extensive training and education
+          to perform various surgeries, from routine procedures to complex
+          life-saving operations. Their expertise and steady hands are vital in
+          improving patients' health and saving lives.
+        </p>
+      </div>
+
+      <div className="mt-12">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold ">
+          Education
+        </h3>
+
+        <ul className="pt-4 md:p-5">
+          <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
+            <div>
+              <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
+                {formateDate("09-16-2017")} - {formateDate("07-04-2023")}
+              </span>
+              <p className="text-[16px] leading-6 font-medium text-textColor">
+                PHD in Surgeon
+              </p>
+            </div>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              New Apollo Hospital, New York
+            </p>
+          </li>
+          <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
+            <div>
+              <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
+                {formateDate("1-23-2011")} - {formateDate("08-12-2016")}
+              </span>
+              <p className="text-[16px] leading-6 font-medium text-textColor">
+                Masters in Surgeon
+              </p>
+            </div>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Hospital of Cincinatti , Cincinatti
+            </p>
+          </li>
+        </ul>
+      </div>
+
+       
+    </div>
+  );
+};
--- src/pages/Doctors/DoctorDetails.jsx
+++ src/pages/Doctors/DoctorDetails.jsx
@@ -1,25 +1,73 @@
-import doctorImage from '../../assets/images/doctor-img02.png'
-
-
+import { useState } from "react";
+import doctorImage from "../../assets/images/doctor-img02.png";
+import starIcon from "../../assets/images/Star.png";
+import { DoctorAbout } from "./DoctorAbout";
+import { Feedback } from "./Feedback";
 export const DoctorDetails = () => {
+  const [tab, setTab] = useState("about");
   return (
     <section>
       <div className="max-w-[1170px] px-5 mx-auto">
         <div className="grid md:grid-cols-3 gap-[50px]">
           <div className="md:col-span-2">
-            <div className='flex items-center gap-5'>
-            <figure className='max-w-[200px] max-h-[200px]'>
-              <img src={doctorImage} alt="" className='w-full' />
-            </figure>
+            <div className="flex items-center gap-5">
+              <figure className="max-w-[200px] max-h-[200px]">
+                <img src={doctorImage} alt="" className="w-full" />
+              </figure>
 
-            <div>
-              <span className='bg-[#CCFOF3] text-irisBlueColor py-1 px-6 lg:py-2 lg:px-6 text-[12px]'>Surgeon</span>
-              <h3 className='text-headingColor text-[22px] leading-9 mt-3 font-bold'>Martin ISHIMWE</h3>
+              <div>
+                <span className="bg-[#CCFOF3] text-irisBlueColor py-1 px-6 lg:py-2 lg:px-6 text-[12px]">
+                  Surgeon
+                </span>
+                <h3 className="text-headingColor text-[22px] leading-9 mt-3 font-bold">
+                  Martin ISHIMWE
+                </h3>
+                <div className="flex items-center gap-[6px]">
+                  <span className="flex items-center gap-[6px] text-[14px] leading-5 lg:leading-7 font-semibold text-headingColor">
+                    <img src={starIcon} alt="" />
+                    4.8
+                  </span>
+                  <span className="flex items-center gap-[6px] text-[14px] leading-5 lg:leading-7 font-semibold text-headingColor">
+                    (272)
+                  </span>
+                </div>
+                <p className="text__para text-[14px] leading-5 md:text-[15px] lg:max-w-[390px]">
+                  A surgeon specialist doctor is a highly trained and skilled
+                  medical professional who specializes in performing precise
+                  surgical procedures in their specific field of expertise. They
+                  diagnose, treat, and manage various medical conditions through
+                  surgery, playing a crucial role in healthcare.
+                </p>
+              </div>
             </div>
+
+            <div className="mt-[50px] border-b border-solid border-[#0066ff34]">
+              <button
+                onClick={() => setTab("about")}
+                className={`${
+                  tab == "about" && "border-b border-solid border-primaryColor"
+                } py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold`}
+              >
+                About
+              </button>
+              <button
+                onClick={() => setTab("feedback")}
+                className={`${
+                  tab == "feedback" &&
+                  "border-b border-solid border-primaryColor"
+                } py-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold`}
+              >
+                Feedbacks
+              </button>
+            </div>
+
+            <div className="mt-[50px]">
+              {tab == "about" && <DoctorAbout />}
+              {tab == "feedback" && <Feedback />}
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
