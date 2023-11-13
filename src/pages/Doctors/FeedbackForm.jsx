636525c00ac50f6162200889702c37da18a08b96
Terence Faid JABO <faidterence@outlook.com>
2023-11-13 15:27:48 +0200

    feat: add feedback text area to FeedbackForm component

--- src/pages/Doctors/FeedbackForm.jsx
+++ src/pages/Doctors/FeedbackForm.jsx
@@ -4,11 +4,16 @@ import { AiFillStar } from "react-icons/ai";
 const FeedbackForm = () => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
+  const [reviewText, setReviewText] = useState("");
+
+  const handleSubmitReview = async (e) => {
+    // Fetch Apis
+  };
 
   return (
     <form>
       <div>
-        <h3 className="text-headingColor text-[16px] leading-6 font-semibold mb-4">
+        <h3 className="text-headingColor text-[16px] leading-6 font-semibold mb-4 mt-0">
           How would you rate the overall Experience? *
         </h3>
         <div>
@@ -39,6 +44,22 @@ const FeedbackForm = () => {
           })}
         </div>
       </div>
+
+      <div className="mt-[30px]">
+        <h3 className="text-headingColor text-[16px] leading-6 font-semibold mb-4 mt-0">
+          Share Your Feedback or Suggestions *
+        </h3>
+        <textarea
+          className="border border-solid border-[#0066ff34] focus:outline outline-primaryColor w-full px-4 py-3 rounded-md"
+          rows="5"
+          placeholder="Write your message"
+          onChange={(e) => setReviewText(e.target.value)}
+        ></textarea>
+      </div>
+
+      <button className="btn" type="submit" onClick={handleSubmitReview}>
+        Give Feedback
+      </button>
     </form>
   );
 };
