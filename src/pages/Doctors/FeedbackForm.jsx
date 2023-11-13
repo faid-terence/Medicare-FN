08cc209cc99d26a5599e41935bcf401877a16bc7
Terence Faid JABO <faidterence@outlook.com>
2023-11-13 15:15:58 +0200

    feat: implement interactive star rating in FeedbackForm component

--- src/pages/Doctors/FeedbackForm.jsx
+++ src/pages/Doctors/FeedbackForm.jsx
@@ -3,24 +3,36 @@ import { AiFillStar } from "react-icons/ai";
 
 const FeedbackForm = () => {
   const [rating, setRating] = useState(0);
-
-  const handleRatingClick = (newRating) => {
-    setRating(newRating);
-  };
+  const [hover, setHover] = useState(0);
 
   return (
-    <form action="">
+    <form>
       <div>
         <h3 className="text-headingColor text-[16px] leading-6 font-semibold mb-4">
           How would you rate the overall Experience? *
         </h3>
         <div>
-          {[...Array(5).keys()].map((index) => {
-            const starRating = index + 1;
+          {[...Array(5).keys()].map((_, index) => {
+            index += 1;
             return (
-              <button key={starRating} type="button" onClick={() => handleRatingClick(starRating)}>
+              <button
+                key={index}
+                type="button"
+                className={`${
+                  index <= (rating || hover)
+                    ? "text-yellowColor"
+                    : "text-gray-400"
+                } bg-transparent border-none outline-none text-[22px] cursor-pointer`}
+                onClick={() => setRating(index)}
+                onMouseEnter={() => setHover(index)}
+                onMouseLeave={() => setHover(rating)}
+                onDoubleClick={() => {
+                  setHover(0);
+                  setRating(0);
+                }}
+              >
                 <span>
-                  {starRating <= rating ? <AiFillStar color="#FFD700" /> : <AiFillStar />}
+                  <AiFillStar />
                 </span>
               </button>
             );
